199a7982264b74ce0070bc8340817f15c89f38ca
Megan Hugo <mhugo@dvtsoftware.com>
2024-02-16 12:18:04 +0200

    feat: :sparkles: Added Leaflet map and markers
    Added Leaflet map to fit the whole screen, and added markers for the major cities in South Africa.

--- SkyGuide/README.md
+++ SkyGuide/README.md
@@ -3,3 +3,4 @@
 SkyGuide is page that shows the weather based on the location the User picks
 
 *it makes use of the 7timer API -https://www.7timer.info*
+*Design inspo from https://pointsnorthstudio.com/blog/deep-dive-if-we-designed-a-weather-app/*
--- SkyGuide/main.js
+++ SkyGuide/main.js
@@ -3,22 +3,30 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const map = L.map('map').setView([-30.5595, 22.9375], 5);
 
-setupCounter(document.querySelector('#counter'))
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
+
+const cities = [
+  [-26.202, 28.044], // Johannesburg
+  [-33.926, 18.423], // Cape Town
+  [-29.858, 31.029], // Durban
+  [-25.745, 28.188], // Pretoria
+  [-33.961, 25.615], // Port Elizabeth
+  [-29.121, 26.214], // Bloemfontein
+  [-33.015, 27.912], // East London
+  [-29.617, 30.393],  // Pietermaritzburg
+  [-26.268, 27.858],  // Soweto
+  [-26.188, 28.321], // Bononi
+  [-25.996, 28.227], // Tembisa
+  [-26.673, 27.926] // Vereeniging
+  
+];
+
+cities.forEach(city => {
+  L.marker(city).addTo(map); 
+});
+  
