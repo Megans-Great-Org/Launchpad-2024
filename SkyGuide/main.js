bb567647ba9939e9527c682cd91392e160c711a3
Megan Hugo <mhugo@dvtsoftware.com>
2024-02-17 11:37:27 +0200

    feat: :sparkles: Added dynamic data for weather pop-up
    The Pop-up now gets populated from the API dynamically when a pin location is clicked.

--- SkyGuide/main.js
+++ SkyGuide/main.js
@@ -11,22 +11,84 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 const cities = [
-  [-26.202, 28.044], // Johannesburg
-  [-33.926, 18.423], // Cape Town
-  [-29.858, 31.029], // Durban
-  [-25.745, 28.188], // Pretoria
-  [-33.961, 25.615], // Port Elizabeth
-  [-29.121, 26.214], // Bloemfontein
-  [-33.015, 27.912], // East London
-  [-29.617, 30.393],  // Pietermaritzburg
-  [-26.268, 27.858],  // Soweto
-  [-26.188, 28.321], // Bononi
-  [-25.996, 28.227], // Tembisa
-  [-26.673, 27.926] // Vereeniging
-  
+  { lat: -26.202, lng: 28.044, city_name: "JOHANNESBURG" }, 
+  { lat: -33.926, lng: 18.423, city_name: "CAPE TOWN" },
+  { lat: -29.858, lng: 31.029, city_name: "DURBAN" },
+  { lat: -25.745, lng: 28.188, city_name: "PRETORIA" },
+  { lat: -33.961, lng: 25.615, city_name: "PORT ELIZABETH" },
+  { lat: -29.121, lng: 26.214, city_name: "BLOEMFONTEIN" },
+  { lat: -33.015, lng: 27.912, city_name: "EAST LONDON" },
+  { lat: -29.617, lng: 30.393, city_name: "PIETERMARITZBURG" },
+  { lat: -26.268, lng: 27.858, city_name: "SOWETO" },
+  { lat: -26.188, lng: 28.321, city_name: "BONONI" },
+  { lat: -25.996, lng: 28.227, city_name: "TEMBISA" },
+  { lat: -26.673, lng: 27.926, city_name: "VEREENIGING" }
 ];
 
+const weatherConditions = [
+  { code: 0, condition: 'Clear' },
+  { code: 1, condition: 'Partly Cloudy' },
+  { code: 2, condition: 'Cloudy' },
+  { code: 3, condition: 'Overcast' },
+  { code: 4, condition: 'Fog' },
+  { code: 5, condition: 'Freezing Fog' },
+  { code: 6, condition: 'Drizzle' },
+  { code: 7, condition: 'Rain' },
+  { code: 8, condition: 'Freezing Rain' },
+  { code: 9, condition: 'Snow' },
+  { code: 10, condition: 'Heavy Snow' },
+  { code: 11, condition: 'Sleet' },
+];
+
+
+
 cities.forEach(city => {
-  L.marker(city).addTo(map); 
+  const marker = L.marker(city).addTo(map); 
+
+  //sets the marker to display the weather for the city when clicked
+  marker.on('click', function() {
+    const { lat, lng } = marker.getLatLng();
+
+
+  const params = {
+    "latitude": lat,
+    "longitude": lng,
+    "current": ["temperature_2m", "weather_code"],
+    "hourly": ["temperature_2m", "weather_code"],
+    "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min"],
+    "timezone": "Africa/Cairo",
+    "forecast_days": 1
+  };
+  const queryString = new URLSearchParams(params).toString();
+  const url = `https://api.open-meteo.com/v1/forecast?${queryString}`;
+
+  fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+      console.log(data);
+
+      const name = document.getElementsByClassName("city-name");
+      name[0].innerText = city.city_name;
+
+      const temperature = document.getElementsByClassName("temperature");
+      temperature[0].innerText = `${Math.round(data.current.temperature_2m)}*`;
+
+      const weather = document.getElementsByClassName("weather");
+      weather[0].innerText = weatherConditions[data.current.weather_code].condition;
+
+      const range = document.getElementsByClassName("temperature-range");
+      range[0].innerText = `L:  ${Math.round(data.daily.temperature_2m_min)}* H:${Math.round(data.daily.temperature_2m_max)}*`;
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+  });
 });
   
