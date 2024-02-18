fcad1464c0c4e71f64e04e99d727d2e4ac55706a
Megan Hugo <mhugo@dvtsoftware.com>
2024-02-18 10:52:17 +0200

    feat: :sparkles: Added on click functions for list
    For the list of cities, an onclick event was added to each so that you can pick a city from them and it will display the weather for that city.

--- SkyGuide/main.js
+++ SkyGuide/main.js
@@ -52,56 +52,23 @@ cities.forEach(city => {
   const a = document.createElement("a");
   a.href = "#"; //remember to set href
   a.innerText = city.city_name;
+
+  a.addEventListener("click", function(event) {
+    event.preventDefault(); 
+    //close the nav
+    map.setView([city.lat, city.lng], map.getZoom()); 
+    getWeatherData(city);
+});
+
   cityList.appendChild(a);
   
   marker.on('click', function() {
     const { lat, lng } = marker.getLatLng();
 
+    map.setView([lat, lng], map.getZoom());
 
-    const params = {
-      "latitude": lat,
-      "longitude": lng,
-      "current": ["temperature_2m", "weather_code"],
-      "hourly": ["temperature_2m", "weather_code"],
-      "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min"],
-      "timezone": "Africa/Cairo",
-      "forecast_days": 1
-    };
-    const queryString = new URLSearchParams(params).toString();
-    const url = `https://api.open-meteo.com/v1/forecast?${queryString}`;
-
-    fetch(url)
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(data => {
-        console.log(data);
-
-        const name = document.getElementsByClassName("city-name");
-        name[0].innerText = city.city_name;
-
-        const temperature = document.getElementsByClassName("temperature");
-        temperature[0].innerText = `${Math.round(data.current.temperature_2m)}*`;
-
-        const weather = document.getElementsByClassName("weather");
-        weather[0].innerText = weatherConditions[data.current.weather_code].condition;
-
-        const locationContainer = document.getElementsByClassName("location-container");
-        locationContainer[0].style.backgroundImage = `url(${weatherConditions[data.current.weather_code].image})`;
-
-        const weatherCircle = document.getElementsByClassName("weather-circle");
-        weatherCircle[0].style.backgroundColor = weatherConditions[data.current.weather_code].colour;
-
-        const range = document.getElementsByClassName("temperature-range");
-        range[0].innerText = `L:  ${Math.round(data.daily.temperature_2m_min)}* H:${Math.round(data.daily.temperature_2m_max)}*`;
-
-      })
-      .catch(error => {
-          console.error('Error fetching data:', error);
-      });
+    getWeatherData(city);
+    
 
   });
 });
@@ -136,4 +103,50 @@ document.getElementById('drop-pin').addEventListener('click', function() {
   }
 });
 
+function getWeatherData(city) {
+  const params = {
+    "latitude": city.lat,
+    "longitude": city.lng,
+    "current": ["temperature_2m", "weather_code"],
+    "hourly": ["temperature_2m", "weather_code"],
+    "daily": ["weather_code", "temperature_2m_max", "temperature_2m_min"],
+    "timezone": "Africa/Cairo",
+    "forecast_days": 1
+  };
+  const queryString = new URLSearchParams(params).toString();
+  const url = `https://api.open-meteo.com/v1/forecast?${queryString}`;
 
+  fetch(url)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+      console.log(data);
+
+      const name = document.getElementsByClassName("city-name");
+      name[0].innerText = city.city_name;
+
+      const temperature = document.getElementsByClassName("temperature");
+      temperature[0].innerText = `${Math.round(data.current.temperature_2m)}*`;
+
+      const weather = document.getElementsByClassName("weather");
+      weather[0].innerText = weatherConditions[data.current.weather_code].condition;
+
+      const locationContainer = document.getElementsByClassName("location-container");
+      locationContainer[0].style.backgroundImage = `url(${weatherConditions[data.current.weather_code].image})`;
+
+      const weatherCircle = document.getElementsByClassName("weather-circle");
+      weatherCircle[0].style.backgroundColor = weatherConditions[data.current.weather_code].colour;
+
+      const range = document.getElementsByClassName("temperature-range");
+      range[0].innerText = `L:  ${Math.round(data.daily.temperature_2m_min)}* H:${Math.round(data.daily.temperature_2m_max)}*`;
+
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+}
