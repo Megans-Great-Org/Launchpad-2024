234256d772d6fa953fb6b05ec67bfaccb681da29
Megan Hugo <mhugo@dvtsoftware.com>
2024-02-22 13:22:26 +0200

    Practiced Tailwind

--- rxjs-in-practice/src/main.ts
+++ rxjs-in-practice/src/main.ts
@@ -2,7 +2,7 @@ import { fromFetch } from 'rxjs/fetch';
 import './style.css'
 import { catchError, map, retry, switchMap } from 'rxjs/operators';
 import { fromPromise } from 'rxjs/internal/observable/innerFrom';
-import { EMPTY, fromEvent } from 'rxjs';
+import { EMPTY, Subject, fromEvent } from 'rxjs';
 import { formatDistance, parse } from 'date-fns';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = ` 
@@ -10,6 +10,26 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     Hello
   </div>
 `
+
+//DOMException.jd
+export function updateTheDOMHere(res: any) {
+}
+
+//api.js
+export const mySubject = new Subject();
+export const myAPIResponse = mySubject.pipe(switchMap(() => fromFetch('https')));
+
+//main.js
+function onCLickHandler(evt: Event) {
+  mySubject.next(evt);
+}
+
+myAPIResponse.subscribe(res => {
+  updateTheDOMHere(res);
+})
+
+//Diffrent Example
+
 const observable = fromEvent(document.body, 'click').pipe(
   switchMap(() =>
     fromFetch("https://go-kart-api.onrender.com/runs/SN2780_210722_11H00_NADINE_IDUBE_RACEWAY_16_5554").pipe(
