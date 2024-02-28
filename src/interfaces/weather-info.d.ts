4d46d7e0f2cc0c401a6097e46901cfc52cb1aae4
Megan Hugo <mhugo@dvtsoftware.com>
2024-02-28 10:35:57 +0200

    Revert "Revert "style: :art: Ran prettier and formatted code""
    This reverts commit 8ec2f42c357b58c16bdb47298266ae32fe655c60.

--- src/interfaces/weather-info.d.ts
+++ src/interfaces/weather-info.d.ts
@@ -1,22 +1,21 @@
 export interface WeatherInfoJsonInterface {
-    [key: string]: {
-      day: {
-        description: string;
-        image: string;
-      };
-      night: {
-        description: string;
-        image: string;
-      };
-      designNumber: number;
-    };
-  }
-  
-  export interface WeatherDesignJsonInterface {
-    [key: string]: {
+  [key: string]: {
+    day: {
+      description: string;
       image: string;
-      colour: string;
-      blendColour: string;
     };
-  }
-  
+    night: {
+      description: string;
+      image: string;
+    };
+    designNumber: number;
+  };
+}
+
+export interface WeatherDesignJsonInterface {
+  [key: string]: {
+    image: string;
+    colour: string;
+    blendColour: string;
+  };
+}
